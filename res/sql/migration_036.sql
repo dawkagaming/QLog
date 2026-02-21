f18bba0ef71410228b68a5f393f9892755d49a68
Ladislav Foldyna <foldyna@gmail.com>
2026-02-21 19:13:43 +0100

    QSO Filter: Added REGEXP

--- res/sql/migration_036.sql
+++ res/sql/migration_036.sql
@@ -3,3 +3,5 @@ ALTER TABLE rig_profiles ADD COLUMN rigctld_port INTEGER DEFAULT 4532;
 ALTER TABLE rig_profiles ADD COLUMN rigctld_path TEXT;
 ALTER TABLE rig_profiles ADD COLUMN rigctld_args TEXT;
 ALTER TABLE contacts_qsl_cards ADD COLUMN favorite INTEGER DEFAULT 0;
+
+INSERT INTO qso_filter_operators VALUES(7, "regexp");
